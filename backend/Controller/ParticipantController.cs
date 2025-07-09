b1268a06f831ecbc6125ef29101db989f739ac62
Tiffani <yho777@aucklanduni.ac.nz>
2025-07-09 17:36:33 +1200

    feat: Add Participant CRUD APIs

---
+++ backend/Controller/ParticipantController.cs
@@ -0,0 +1,153 @@
+﻿using Azure.Core;
+using backend.Data;
+using backend.Models.Entity;
+using backend.Models.Enum;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controller
+{
+    [ApiController]
+    [Route("api/participant")]
+    public class ParticipantController:ControllerBase
+    {
+        private ApplicationDbContext _db;
+
+        public ParticipantController (ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost("join/{mealId}")]
+        public async Task<IActionResult> JoinMeal(int mealId, int userId)
+        {
+            try
+            {
+                var meal = await _db.Meals.FindAsync(mealId);
+                if (meal == null)
+                {
+                    return NotFound();
+                }
+
+                if (meal.CurrentParticipant == meal.MaxParticipant)
+                {
+                    return StatusCode(409, new { message = "Meal is already full" });
+                }
+
+                if (await _db.MealParticipants.AnyAsync(p => p.MealId == mealId && p.UserId == userId))
+                {
+                    return StatusCode(422, new { message = "User already joined this meal." });
+                }
+
+                var participant = new MealParticipant
+                {
+                    MealId = mealId,
+                    UserId = userId,
+                    ParticipationStatus = MealParticipateStatus.joined,
+                    JoinedAt = DateTimeOffset.UtcNow
+                };
+
+                _db.MealParticipants.Add(participant);
+                await _db.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Error in joining meal: ", ex.Message });
+            }
+        }
+
+        [HttpDelete("leave/{mealId}")]
+        public async Task<IActionResult> LeaveMeal (int mealId, int userId)
+        {
+            try
+            {
+                var participant = await _db.MealParticipants
+                    .FirstOrDefaultAsync(mp => mp.MealId == mealId && mp.UserId == userId);
+
+                if (participant == null)
+                {
+                    return NotFound(new { message = "Not a participant of this meal" });
+                }
+
+                _db.MealParticipants.Remove(participant);
+
+                var meal = await _db.Meals.FindAsync(mealId);
+                if (meal != null)
+                {
+                    meal.CurrentParticipant = Math.Max(1, meal.CurrentParticipant - 1);
+                }
+
+                await _db.SaveChangesAsync();
+
+                return Ok(new { message = "Successfully left meal" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Error in leaving the meal: ", ex.Message });
+            }
+        }
+
+        [HttpPatch("status/{mealId}/{userId}")]
+        public async Task<IActionResult> UpdateParticipantStatus (int mealId, int userId, [FromBody] MealParticipateStatus status)
+        {
+            try
+            {
+                var participant = await _db.MealParticipants
+                    .FirstOrDefaultAsync(mp => mp.MealId == mealId && mp.UserId == userId);
+
+                if (participant == null)
+                {
+                    return NotFound(new { message = "Participant not found" });
+                }
+
+                participant.ParticipationStatus = status;                    
+                await _db.SaveChangesAsync();
+
+                return Ok(new { message = "Participant status updated", status = status.ToString() });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Error updating status", error = ex.Message });
+            }
+        }
+
+        [HttpGet("meal/{mealId}")]
+        public async Task<IActionResult> GetParticipantsForMeal (int mealId)
+        {
+            try
+            {
+                var participants = await _db.MealParticipants
+                    .Include(p => p.User)
+                    .Where(p => p.MealId == mealId)
+                    .ToListAsync();
+
+                return Ok(participants);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Error in getting participants of a meal: ", ex.Message });
+            }
+        }
+
+        [HttpGet("user/{userId}")]
+        public async Task<IActionResult> GetMealForUser (int userId)
+        {
+            try
+            {
+                var meals = await _db.MealParticipants
+                    .Include(p => p.Meal)
+                    .Where(p => p.UserId == userId)
+                    .ToListAsync();
+
+                return Ok(meals);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Error in getting meals of a user: ", ex.Message });
+            }
+        }
+    }
+}
