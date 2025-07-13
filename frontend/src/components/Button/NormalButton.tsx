31ebcc6f75aa9d7819b08016a71c5e7da660c82a
Tiffani <yho777@aucklanduni.ac.nz>
2025-07-13 13:00:42 +1200

    feat(button): Add normal button and modify submit button hover animation

---
+++ frontend/src/components/Button/NormalButton.tsx
@@ -0,0 +1,37 @@
+function NormalButton({
+  message,
+
+  onClick,
+}: {
+  message?: string;
+  onClick?: () => void;
+}) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="
+        w-full
+        py-2
+        rounded-lg
+        border-2
+        border-rose-300
+        bg-white
+        bg-clip-padding
+        bg-gradient-to-r from-rose-400 to-rose-300
+        text-transparent
+        bg-clip-text
+        font-semibold
+        transition-all transform
+        hover:scale-105
+        hover:shadow-lg
+        hover:bg-gradient-to-r hover:from-rose-400 hover:to-rose-300
+        focus:ring-2 focus:ring-rose-300
+      "
+    >
+      {message}
+    </button>
+  );
+}
+
+export default NormalButton;
--- frontend/src/components/Button/SubmitButton.tsx
+++ frontend/src/components/Button/SubmitButton.tsx
@@ -2,7 +2,19 @@ function SubmitButton({ message }: { message?: string }) {
   return (
     <button
       type="submit"
-      className="w-full bg-gradient-to-r from-rose-400 to-rose-300 text-white py-2 rounded-lg hover:scale-105 transition-colors"
+      className="
+  w-full 
+  py-2 
+  rounded-lg 
+  bg-gradient-to-r from-rose-400 to-rose-300
+   hover:from-rose-300 hover:to-rose-400
+  text-white 
+  font-semibold
+  transition-all transform
+  hover:scale-105
+  hover:shadow-lg
+  focus:ring-2 focus:ring-rose-300
+"
     >
       {message}
     </button>
