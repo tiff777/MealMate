ca2a442ce0e861870878c10db2daebf563f81cad
Tiffani <yho777@aucklanduni.ac.nz>
2025-07-17 18:38:15 +1200

    feat(meal): Add meal id in ChatRoom entity

--- backend/Models/Entity/ChatRoom.cs
+++ backend/Models/Entity/ChatRoom.cs
@@ -5,10 +5,12 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
         public int HostId { get; set; } 
         public bool IsPrivate { get; set; } = false;
 
+        public int? MealId { get; set; }
+        public virtual Meal? Meal { get; set; }
 
         public virtual ICollection<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
         public virtual ICollection<ChatRoomMember> Members { get; set; } = new List<ChatRoomMember>();
